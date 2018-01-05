d333d8f53908e29e881079615459109942ee010b
Vitor A. Vale <vitoravale@outlook.com>
2018-01-04 22:27:40 -0200

    start rout guard

--- src/router/index.js
+++ src/router/index.js
@@ -7,17 +7,46 @@ function load(component) {
   return () => System.import(`../components/${component}.vue`);
 }
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
+      name: 'Dashboard',
+      component: load('Dashboard'),
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: '/login',
       name: 'Login',
       component: load('Login'),
+      meta: {
+        requiresAuth: false,
+      },
     },
     {
       path: '*',
       name: 'Error',
       component: load('Error'),
+      meta: {
+        requiresAuth: false,
+      },
     },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    const auth = false;
+    if (!auth) {
+      next({ path: '/login', query: { redirect: to.fullPath } });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
